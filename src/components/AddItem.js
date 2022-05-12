ae674084fc2d9872af8534b382a4d91a775c7be3
Alex Orlet <AlexOrlet89@gmail.com>
2022-05-11 17:27:39 -0700

    useReducer called, default state established.

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 export default function AddItem() {
+  //we are going to use useReducer to keep the state of our shopping list items. the initial value is going to have just text to begin with: eggs.
+
+  const initialItems = [{ text: 'Eggs' }];
+  const [items, dispatch] = useReducer(itemReducer, initialItems);
+
   return (
     <>
       <form>
