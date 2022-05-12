59a4e76eea14e3af80d3d5db8d3cdf81d3181956
Alex Orlet <AlexOrlet89@gmail.com>
2022-05-11 17:57:08 -0700

    newItem useState, handleAddItem sets newItem, next up is to create the itemReducer action type

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,15 +1,30 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useState } from 'react';
 
 export default function AddItem() {
   //we are going to use useReducer to keep the state of our shopping list items. the initial value is going to have just text to begin with: eggs.
 
+  const itemReducer = () => {
+    console.log('that thang');
+  };
+
+  const [newItem, setNewItem] = useState('');
   const initialItems = [{ text: 'Eggs' }];
   const [items, dispatch] = useReducer(itemReducer, initialItems);
 
+  const handleAddItem = (e) => {
+    e.preventDefault();
+    // dispatch({ type: 'ADD_ITEM', payload: { item } });
+    console.log(newItem);
+  };
+
   return (
     <>
-      <form>
-        <input type="text"></input>
+      <form onSubmit={handleAddItem}>
+        <input
+          type="text"
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+        ></input>
         <button>Submit</button>
       </form>
     </>
