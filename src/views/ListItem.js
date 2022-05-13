b4641bc90bacea3ded4e413d4d7640b91202ec58
Alex Orlet <AlexOrlet89@gmail.com>
2022-05-13 09:31:20 -0700

    edit button sets isediting

--- src/views/ListItem.js
+++ src/views/ListItem.js
@@ -3,7 +3,11 @@ import { useItems } from '../context/ItemProvider';
 
 export default function ListItem({ item }) {
   const { handleDeleteItem } = useItems();
-  const [isEditing, setIsEditing] = useState(true);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEditButton = () => {
+    setIsEditing(true);
+  };
 
   let content;
 
@@ -24,7 +28,7 @@ export default function ListItem({ item }) {
         {/* for instance, {item.text} {item.id} */}
         {content}
         <button onClick={() => handleDeleteItem(item.id)}>Delete</button>
-        <button>Edit</button>
+        <button onClick={handleEditButton}>Edit</button>
         <input type="checkbox"></input>
       </div>
     </>
